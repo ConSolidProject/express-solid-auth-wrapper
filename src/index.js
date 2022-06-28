1c120584b35282c4e597f3eb3c2b25257da3d061
jmauwerb <jeroen.werbrouck@ugent.be>
2022-06-28 13:15:01 +0200

    extract webid from bearer token

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-var { createSolidTokenVerifier } = require('@solid/access-token-verifier')
+var { createSolidTokenVerifier} = require('@solid/access-token-verifier')
+const {generateFetch} = require('./auth')
+const {verifySolidAccessToken} = require('@solid/access-token-verifier/dist/algorithm/verifySolidAccessToken')
 var { SolidNodeClient } = require('solid-node-client')
 var solidOidcAccessTokenVerifier = createSolidTokenVerifier()
 
@@ -19,8 +21,8 @@ var solidOidcAccessTokenVerifier = createSolidTokenVerifier()
 function setSatellite(config) {
     return async function(req, res, next) {
         try {
-            
-            const { email, password, idp } = JSON.parse(config)
+            console.log('config', config)
+            const { email, password, idp } = config
             if (email && password) {
                 req.fetch = await generateFetch(email, password, idp)
             } else {
@@ -37,29 +39,50 @@ function setSatellite(config) {
 async function extractWebId(req, res, next) {
     const authorizationHeader = req.headers.authorization
     const dpopHeader = req.headers.dpop
-    const method = req.method
-    var url = req.protocol + '://' + req.get('host') + req.originalUrl;
- 
-    try {
-       const { client_id: clientId, webid: webId } = await solidOidcAccessTokenVerifier(
-          authorizationHeader,
-          {
-             header: dpopHeader,
-             method,
-             url
-          }
-       );
-          req.auth = {}
-       req.auth.webId = webId
-       req.auth.clientId = clientId
-       next()
-    } catch (error) {
-       const message = `Error verifying Access Token via WebID: ${error}}`;
-       console.log(message)
-       req.auth = {}
-       req.auth.webId = undefined
-       req.auth.clientId = undefined
-       next()
+    const auth = {}
+
+    if (dpopHeader) {
+        try {
+            const method = req.method
+            var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+            const { client_id: clientId, webid: webId } = await solidOidcAccessTokenVerifier(
+               authorizationHeader,
+               {
+                  header: dpopHeader,
+                  method,
+                  url
+               }
+            );
+            auth.webId = webId
+            auth.clientId = clientId
+            req.auth = auth
+            next()
+         } catch (error) {
+            const message = `Error verifying Access Token via WebID: ${error}}`;
+            console.log(message)
+            auth.webId = undefined
+            auth.clientId = undefined
+            req.auth = autch
+            next()
+         }
+    } else if (authorizationHeader.startsWith("Bearer")) {
+        try {
+            const validation = await verifySolidAccessToken({header: authorizationHeader})
+
+            auth.webId = validation.webid
+            auth.clientId = validation.client_id
+            req.auth = auth
+            next()
+        } catch (error) {
+            console.log('error', error)
+           next(error) 
+        }
+    } else {
+        
+        auth.webId = undefined
+        auth.clientId = undefined
+        req.auth = auth
+        next()
     }
 }
 
